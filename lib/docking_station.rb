49015253a6cd1d52efe3c682e46246d7c4e532aa
anu <anu_iv2002@hotmail.com>
2019-02-06 15:32:04 +0000

    question10

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,3 +1,13 @@
 class DockingStation
 
+def release_bike
+  new_bike = Bike.new
+end
+end
+
+class Bike
+
+  def working?
+    return true
+  end
 end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,4 +1,12 @@
 require "docking_station"
 describe DockingStation do
+  it {is_expected.to respond_to :release_bike}
 
+
+
+    it "can get a bike and its working" do
+      station = DockingStation.new
+      bike = station.release_bike
+      expect(bike.working?).to eq true
+    end
 end
