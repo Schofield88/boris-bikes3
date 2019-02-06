4c4d6b24b90acb35d6ce6ec84e34fc1e0674681d
Luke Phyall <lukeprospect@yahoo.com>
2019-02-06 15:55:37 +0000

    Adds docking respond test.

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -3,6 +3,7 @@ class DockingStation
 def release_bike
   new_bike = Bike.new
 end
+
 end
 
 class Bike
@@ -10,4 +11,5 @@ class Bike
   def working?
     return true
   end
+
 end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -2,11 +2,16 @@ require "docking_station"
 describe DockingStation do
   it {is_expected.to respond_to :release_bike}
 
-
-
-    it "can get a bike and its working" do
+  it "can get a bike and its working" do
       station = DockingStation.new
       bike = station.release_bike
       expect(bike.working?).to eq true
     end
+
+  it 'can make docking possible' do
+    red_bike = subject.release_bike
+    expect(red_bike).to respond_to :dock
+  end
+
+
 end
